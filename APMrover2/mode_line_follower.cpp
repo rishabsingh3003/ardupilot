17d08ba93016536d8f5e810cc60fc5a655c8f6b4
Rishabh Singh <f20171602@hyderabad.bits-pilani.ac.in>
2019-09-12 13:30:55 +0530

    Correction in GCS msg

--- APMrover2/mode_line_follower.cpp
+++ APMrover2/mode_line_follower.cpp
@@ -5,9 +5,10 @@
 void ModeLineFollower::update()
 {   float desired_steering, desired_throttle;
     get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
-    gcs().send_text(MAV_SEVERITY_INFO, "Throttle set to %5.3f")(float)desired_throttle;
+    gcs().send_text(MAV_SEVERITY_INFO, "Throttle set to %5.3f",(double)desired_throttle);
     g2.motors.set_throttle(desired_throttle);
     ///turn clockwise
+float steering_out;
      steering_out = attitude_control.get_steering_out_rate(4500.0f,
                                                               g2.motors.limit.steer_left,
                                                               g2.motors.limit.steer_right,
