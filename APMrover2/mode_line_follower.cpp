6ecc54abbeabb3bbe867ff51ae3ec03173d89a42
rishabsingh3003 <f20171602@hyderabad.bits-pilani.ac.in>
2019-09-12 18:30:34 +0530

    Intial get_steering_out_rate test
    Testing Acro mode turn method for line follower

--- APMrover2/mode_line_follower.cpp
+++ APMrover2/mode_line_follower.cpp
@@ -1,7 +1,17 @@
 #include "mode.h"
 #include "Rover.h"
+#include <GCS_MAVLink/GCS.h>
 
 void ModeLineFollower::update()
-{
-    g2.motors.set_throttle(100.0f);
+{   float desired_steering, desired_throttle;
+    get_pilot_desired_steering_and_throttle(desired_steering, desired_throttle);
+    gcs().send_text(MAV_SEVERITY_INFO, "Throttle set to %5.3f")(float)desired_throttle;
+    g2.motors.set_throttle(desired_throttle);
+    ///turn clockwise
+     steering_out = attitude_control.get_steering_out_rate(4500.0f,
+                                                              g2.motors.limit.steer_left,
+                                                              g2.motors.limit.steer_right,
+                                                              rover.G_Dt);
+    set_steering(steering_out * 4500.0f);
+
 }
