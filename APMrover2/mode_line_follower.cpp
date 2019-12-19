62bb52243ffc22a7aa45a99f24fd21cb1cb105ae
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2019-12-19 15:57:14 +0530

    New line follower mode

--- APMrover2/mode_line_follower.cpp
+++ APMrover2/mode_line_follower.cpp
@@ -2,27 +2,17 @@
 #include "Rover.h"
 #include <GCS_MAVLink/GCS.h>
 
-void ModeLineFollower::update()
-{   float desired_steering, desired_throttle,steering_out,IR_Reading;
+void ModeLineFollower::update(){   
+    
+    float desired_steering, desired_throttle,steering_out,ir_reading;
     
     get_pilot_input(desired_steering, desired_throttle);
-   
     g2.motors.set_throttle(desired_throttle);
-   
-    //set analog pins 
-    AR_IR* IR2 = new AR_IR;
-    IR_Reading= IR2->readIR(16);// pin 16 defined on SITL Analog input 
-    delete IR2;
-    gcs().send_text(MAV_SEVERITY_INFO, "IR value %5.3f",(double)IR_Reading);
-      
-    const float target_turn_rate = (-IR_Reading) * radians(g2.acro_turn_rate);
-    {
-        steering_out = attitude_control.get_steering_out_rate(target_turn_rate,
-                                                              g2.motors.limit.steer_left,
-                                                              g2.motors.limit.steer_right,
-                                                              rover.G_Dt);
-    }
+    
+    g2.ir_sensor.read();
+    g2.ir_sensor.calc_line_error(ir_reading); //read IR sensor values
 
- 
+    const float target_turn_rate = (-ir_reading) * radians(40);
+    steering_out = attitude_control.get_steering_out_rate(target_turn_rate, g2.motors.limit.steer_left, g2.motors.limit.steer_right, rover.G_Dt);
     set_steering(steering_out * 4500.0f);
 }
