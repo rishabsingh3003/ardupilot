445a728ab271d784fd17ae79a223eba41c43751b
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2019-12-19 15:20:40 +0530

    simulated ir sensor

--- libraries/AP_HAL_SITL/sitl_irsensor.cpp
+++ libraries/AP_HAL_SITL/sitl_irsensor.cpp
@@ -14,7 +14,6 @@
 #include "SITL_State.h"
 #include <SITL/SITL.h>
 #include <AP_Math/AP_Math.h>
-#include <GCS_MAVLink/GCS.h>
 #include <AP_Common/Location.h>
 #include <AP_AHRS/AP_AHRS.h>
 
@@ -22,46 +21,52 @@ extern const AP_HAL::HAL& hal;
 
 using namespace HALSITL;
 
-void SITL_State::_update_irsensor() 
-{   
-    const Location &home_loc = AP::ahrs().get_home();
-    Location projected_home_line;
-    Location current_loc;   
+void SITL_State::_update_irsensor(){
+
+    const Location &home_loc = AP::ahrs().get_home();// home location
+    Location projected_home_line; //virtual projected line from home
+    Location sensor_1; //location of first sensor
+    Location sensor_2;// Location of second sensor
+    
+    float current_heading =  AP::ahrs().yaw_sensor * 0.01f;
+    float max_sensor_range = 300; // maximum range of the sensor (cm)
 
     projected_home_line.lat = home_loc.lat;
     projected_home_line.lng = home_loc.lng;
-    current_loc.lat = _sitl->state.latitude*1.0e7;
-    current_loc.lng = _sitl->state.longitude*1.0e7;
-    projected_home_line.offset_bearing(0,1000);
+    projected_home_line.offset_bearing(0,1000);//1km line projected from home
 
-    Vector2f start_NE, end_NE, current_pos_NE;
+    sensor_1.lat = _sitl->state.latitude*1.0e7;
+    sensor_1.lng = _sitl->state.longitude*1.0e7;
+    sensor_1.offset_bearing(wrap_360(current_heading+90),0.75); //First sensor offset is set at 75cm 
+
+    sensor_2.lat = _sitl->state.latitude*1.0e7;
+    sensor_2.lng = _sitl->state.longitude*1.0e7;
+    sensor_2.offset_bearing(wrap_360(current_heading-90),0.75); //Second sensor offset is set at -75cm
+    
+    Vector2f start_NE, end_NE,sensor_1_offset_NE,sensor_2_offset_NE; //get vectors from origin to the points
     bool home_loc_vector = home_loc.get_vector_xy_from_origin_NE(start_NE);
-    bool projected_line_vector = projected_home_line.get_vector_xy_from_origin_NE(end_NE);
-    bool current_pos_vector = current_loc.get_vector_xy_from_origin_NE(current_pos_NE);
-    float check_side= ((end_NE.x - start_NE.x)*(current_pos_NE.y- start_NE.y)-(end_NE.y - start_NE.y)*(current_pos_NE.x- start_NE.x))/1.0e7;
+    bool projected_line_vector = projected_home_line.get_vector_xy_from_origin_NE(end_NE);    
+    bool sensor_1_vector = sensor_1.get_vector_xy_from_origin_NE(sensor_1_offset_NE);
+    bool sensor_2_vector = sensor_2.get_vector_xy_from_origin_NE(sensor_2_offset_NE); 
 
-    float crosstrack_error = Vector2f::closest_distance_between_line_and_point(start_NE, end_NE, current_pos_NE);
-    int check_side_signum = (check_side > 0) ? check_side = 1 : ((check_side < 0) ? check_side = -1 : check_side = 0);
-    int max_sensor_range = 500;
-    int input_start = -max_sensor_range;
-    int input_end = max_sensor_range;
-    int input_range = input_end - input_start;
-    
-    if(home_loc_vector&projected_line_vector&current_pos_vector)
-    {
-        if ( crosstrack_error >= max_sensor_range)
-        {
-            (check_side_sigum == 1) ? ir_pin_value = 5 : ir_pin_value = 0;
-        }
-        else
-        {
-         ir_pin_value = (crosstrack_error *check_side_signum- input_start)*5/input_range ;}
-        }
-    else
-    {
-        ir_pin_value = 69;
+    float crosstrack_error_1 = Vector2f::closest_distance_between_line_and_point(start_NE, end_NE, sensor_1_offset_NE); // distance between individual sensors and the virtual line
+    float crosstrack_error_2 = Vector2f::closest_distance_between_line_and_point(start_NE, end_NE, sensor_2_offset_NE);
+
+    if (crosstrack_error_1>= max_sensor_range){ //force sensor to stay in range limit
+        crosstrack_error_1 = max_sensor_range;
+    }if(crosstrack_error_2>= max_sensor_range){
+        crosstrack_error_2 = max_sensor_range;
     }
-  
-    
+    float voltage_sensor_1 = linear_interpolate(0,5,crosstrack_error_1,0,max_sensor_range); //convert the distance to voltage
+    float voltage_sensor_2 = linear_interpolate(0,5,crosstrack_error_2,0,max_sensor_range);
+
+     
+    if(home_loc_vector&projected_line_vector&sensor_1_vector&sensor_2_vector){   //check for valid origin
+        ir1_pin_value = voltage_sensor_1*1023/5;
+        ir2_pin_value = voltage_sensor_2*1023/5;
+    }else{
+        ir1_pin_value = 0; //set voltage 0 if valid origin is not available
+        ir2_pin_value = 0;
+    }
 }
 #endif
