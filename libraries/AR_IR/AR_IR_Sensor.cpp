255d0194de13ac06afc368d76336094f910e5e9a
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2019-12-19 15:28:23 +0530

    IR sensor lib frontend cpp

--- libraries/AR_IR/AR_IR_Sensor.cpp
+++ libraries/AR_IR/AR_IR_Sensor.cpp
@@ -1,22 +1,55 @@
 #include <AP_HAL/AP_HAL.h>
 #include "AR_IR_Sensor.h"
+#include "AR_IR_Sensor_Analog.h"
+#include <AP_Math/AP_Math.h>
 #include <GCS_MAVLink/GCS.h>
 
-extern const AP_HAL::HAL& hal ;
+
+const AP_Param::GroupInfo AR_IR::var_info[] = {
+
+// @Param: IR_PIN_1
+// @DisplayName: Infrared Sensor 1 pin number
+// @Description: Analog Pin number to where the first infrared sensor is attached
+// @Range: 0 20
+AP_GROUPINFO("PIN_1", 1, AR_IR, param._ir_1_pin_num, IR_1_PIN_NUM),
+
+// @Param: IR_PIN_2
+// @DisplayName: Infrared Sensor 2 pin number
+// @Description: Analog Pin number to where the second infrared sensor is attached
+// @Range: 0 20
+AP_GROUPINFO("PIN_2", 2, AR_IR, param._ir_2_pin_num, IR_2_PIN_NUM),
+
+// @Param: IR_ADC_VOLTAGE
+// @DisplayName: Infrared Sensor ADC voltage value
+// @Description: ADV voltage value number to where the second infrared sensor is attached
+// @Values: 3.3,5,6.6
+AP_GROUPINFO("ADC_VOLTAGE", 3, AR_IR, param.adc_voltage, 5),
 
 
-AR_IR::AR_IR()
-{
-	_IR_pin_analog_source = hal.analogin->channel(14);
+
+AP_GROUPEND
+}; 
+
+AR_IR::AR_IR(){
+	
+}
+
+void AR_IR::init(){
+	drivers = new AR_IR_Analog(*this, state);
+				
 }
 
 
-float AR_IR::readIR(int sensor_pin)
-{
-int output_start = -1;
-int output_end = 1;
-int output_range = output_end - output_start;
-_IR_pin_analog_source->set_pin(sensor_pin);
-float mapped_output = (_IR_pin_analog_source->read_latest())*output_range / 5 + output_start;
-return mapped_output;
+void AR_IR::read() {
+	drivers->read();
 }
+
+void AR_IR:: calc_line_error(float &line_error){
+	float sensor_error = state.voltage_ir_1 - state.voltage_ir_2; //0v from sensor corresponds to the sensor on black line
+	gcs().send_text(MAV_SEVERITY_CRITICAL, "voltage 1 %10.10f", (double) state.voltage_ir_1);
+	gcs().send_text(MAV_SEVERITY_CRITICAL, "voltage 2 %10.10f", (double) state.voltage_ir_2);
+	line_error = linear_interpolate(-1,1,sensor_error,-param.adc_voltage,param.adc_voltage);
+}
+
+
+	
