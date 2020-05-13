6a65e5caf3bc585f5f4acf8844aca1dcb3b04cd7
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2020-05-13 17:11:04 +0530

    AC_Avoidance: Use 3D data in OA Database

--- libraries/AC_Avoidance/AP_OADatabase.cpp
+++ libraries/AC_Avoidance/AP_OADatabase.cpp
@@ -131,7 +131,7 @@ void AP_OADatabase::update()
 }
 
 // push a location into the database
-void AP_OADatabase::queue_push(const Vector2f &pos, uint32_t timestamp_ms, float distance)
+void AP_OADatabase::queue_push(const Vector3f &pos, uint32_t timestamp_ms, float distance)
 {
     if (!healthy()) {
         return;
@@ -380,7 +380,7 @@ void AP_OADatabase::send_adsb_vehicle(mavlink_channel_t chan, uint16_t interval_
         }
 
         // convert object's position as an offset from EKF origin to Location
-        const Location item_loc(Vector3f(_database.items[idx].pos.x * 100.0f, _database.items[idx].pos.y * 100.0f, 0));
+        const Location item_loc(Vector3f(_database.items[idx].pos.x * 100.0f, _database.items[idx].pos.y * 100.0f, _database.items[idx].pos.z*100.0f));
 
         mavlink_msg_adsb_vehicle_send(chan,
             idx,
