f10884315e1c7b9939afe42b336bd9343f00350b
Rishabh <f20171602@hyderabad.bits-pilani.ac.in>
2021-03-01 00:44:16 +0530

    RC_Channel: provide rc input for lua bindings

--- libraries/RC_Channel/RC_Channel.cpp
+++ libraries/RC_Channel/RC_Channel.cpp
@@ -381,6 +381,26 @@ int16_t RC_Channel::stick_mixing(const int16_t servo_in)
     return servo_out;
 }
 
+/*
+  get the RC input PWM value given a channel number.  Note that
+  channel numbers start at 1, as this API is designed for use in
+  LUA
+*/
+bool RC_Channels::get_pwm(uint8_t c, uint16_t &pwm) const
+{
+    RC_Channel *chan = rc_channel(c-1);
+    if (chan == nullptr) {
+        return false;
+    }
+    int16_t pwm_signed = chan->get_radio_in();
+    if (pwm_signed < 0) {
+        return false;
+    }
+    pwm = (uint16_t)pwm_signed;
+    return true;
+}
+
+
 //
 // support for auxillary switches:
 //
