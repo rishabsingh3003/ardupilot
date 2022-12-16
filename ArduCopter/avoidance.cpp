a91326a0863f1ab22bd07f1c22cf493a8462f53a
rishabsingh3003 <rishabh.singh100358@gmail.com>
2022-12-17 01:35:47 +0530

    Copter: activate avoidnace when rangefinder isn't healthy

--- ArduCopter/avoidance.cpp
+++ ArduCopter/avoidance.cpp
@@ -5,9 +5,16 @@ void Copter::low_alt_avoidance()
 {
 #if AC_AVOID_ENABLED == ENABLED
     int32_t alt_cm;
-    if (!get_rangefinder_height_interpolated_cm(alt_cm)) {
-        // enable avoidance if we don't have a valid rangefinder reading
+    if (is_zero(avoid.get_min_alt())) {
+        // enable avoidance if min alt is zero, i.e this feature is not configured
         avoid.proximity_alt_avoidance_enable(true);
+        return;
+    }
+
+    if (!get_rangefinder_height_interpolated_cm(alt_cm)) {
+        // disable avoidance if we don't have a valid rangefinder reading
+        // this avoids getting avoidance activated when the rangefinder is unhealthy very close to the ground (takeoff and landing)
+        avoid.proximity_alt_avoidance_enable(false);
         return;
     }
 
