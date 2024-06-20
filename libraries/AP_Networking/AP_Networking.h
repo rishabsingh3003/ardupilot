e5786e034b38dff7aa22e9de0f471f2a4d6ceb78
bugobliterator <siddharthbharatpurohit@gmail.com>
2024-06-20 12:30:42 +1000

    AP_Networking: add timeout to swap the UDP Server client target

--- libraries/AP_Networking/AP_Networking.h
+++ libraries/AP_Networking/AP_Networking.h
@@ -269,7 +269,7 @@ private:
         uint16_t last_udp_connect_port;
         bool have_received;
         bool close_on_recv_error;
-
+        uint32_t last_udp_srv_recv_time_ms;
         HAL_Semaphore sem;
     };
 
